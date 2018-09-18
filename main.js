ad9e7d8393b4a40682b9f92a5834bf7e26230261
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-09-18 11:47:19 +0200

    poprawka2

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var n = 99;
 function zbior(){
     if (n>100 && n<201) {
+        return n;
     console.log("Liczba znajduje się w przedziale");
     }else{
     console.log("Liczba nie znajduje się w przedziale");
