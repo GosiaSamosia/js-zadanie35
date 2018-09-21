e43f5bda1e9266fcfb418f36e422f0b94c0804d9
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-09-21 21:58:17 +0200

    popr przekazywanie zmiennej do funkcji

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var n = 99;
-function zbior(){
-    if (n>100 && n<201) {
-    return "Liczba znajduje się w przedziale" ;
-    }else{
-    return "Liczba nie znajduje się w przedziale";
+function zbior(liczba) {
+
+    if ( liczba > 100 && liczba < 201) {
+        return "Liczba znajduje się w przedziale";
+    } else {
+        return "Liczba nie znajduje się w przedziale";
     }
+        return liczba;
 }
-console.log(zbior());
+console.log (zbior(n));
+zbior(n);
